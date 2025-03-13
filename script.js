561c85a4a70293d314e7227beb676781d5bad890
yseuuu <yseu0324@gmail.com>
2025-03-14 05:09:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,86 +1,29 @@
-function hamburg(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform  = "translateY(0px)"
-}
-function cancel(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform  = "translateY(-500px)"
-}
 
+'use strict'
 
+const menuToggle = document.querySelector('.menu-toggle');
+const bxMenu = document.querySelector('.bx-menu');
+const bxX = document.querySelector('.bx-x');
 
-const texts = [
-    "ILLUSTRATOR",
-    "3D ARTIST",
-    "GRAPHIC DESIGNER"
-]
+const navBar = document.querySelector('.navbar');
 
-let speed = 200;
+// --- open menu ---
 
-const textElements = document.querySelector(".typewriter-text")
-
-let textIndex = 0;
-let charcterIndex = 0;
-
-function typeWriter() {
-    if(charcterIndex < texts[textIndex].length){
-        textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
-        charcterIndex++;
-        setTimeout(typeWriter, speed); 
+bxMenu.addEventListener('click', (e)=> {
+    if(e.target.classList.contains('bx-menu')){
+        navBar.classList.add('show-navbar');
+        bxMenu.classList.add('hide-bx');
+        bxX.classList.add('show-bx');
     }
-    else{
-        setTimeout(eraseText, 1000)
+})
+
+// --- close menu ---
+
+bxX.addEventListener('click', (e)=> {
+    if(e.target.classList.contains('bx-x')){
+        navBar.classList.remove('show-navbar');
+        bxMenu.classList.remove('hide-bx');
+        bxX.classList.remove('show-bx');
     }
-}
-
-function eraseText() {
-    if(textElements.innerHTML.length > 0){
-        textElements.innerHTML = textElements.innerHTML.slice(0,-1)
-        setTimeout(eraseText, 50)
-    }
-    else{
-        textIndex = (textIndex + 1) % texts.length;
-        charcterIndex = 0;
-        setTimeout(typeWriter,500)
-    }
-}
-
-window.onload = typeWriter;
-
-
-const dialog = document.querySelector('#dialog');
-const images = document.querySelectorAll('.imageGallery button');
-const closeDialogBtn = document.querySelector('#closeDialogBtn');
-
-function openDialog(index) {
-  if (!document.startViewTransition) {
-    dialog.showModal();
-    document.querySelectorAll('#carousel img')[index].scrollIntoView();
-  } else handleTransition(index);
-}
-
-// Wait until the transition finished and the dialog opens before scrolling into view.
-async function handleTransition(index) {
-  const transition = document.startViewTransition(() => dialog.showModal());
-  try {
-    await transition.finished;
-  } finally {
-    document.querySelectorAll('#carousel img')[index].scrollIntoView();
-  }
-}
-
-const closeDialog = () => {
-  if (!document.startViewTransition) dialog.close();
-  else document.startViewTransition(() => dialog.close());
-};
-
-images.forEach((img, index) => {
-  console.log(index);
-  img.addEventListener('click', () => openDialog(index));
-});
-
-closeDialogBtn.addEventListener('click', () => closeDialog());
-
-
-
+})
 
